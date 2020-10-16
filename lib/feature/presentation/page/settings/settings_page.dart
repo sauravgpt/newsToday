005c13a44f20823104d5011dab7748d028e57b1d
saurav Gupta <sauravgpt34@gmail.com>
2020-10-16 20:06:49 +0530

    Developer profile links added

--- lib/feature/presentation/page/settings/settings_page.dart
+++ lib/feature/presentation/page/settings/settings_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:newstoday/feature/presentation/widget/social_account.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -64,9 +65,51 @@ class _SettingsPageState extends State<SettingsPage> {
                 ),
               ],
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: SocialAccount(),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'Made with ♥ in India',
+                  style: TextStyle(
+                    fontSize: 42.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class SocialAccount extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: Hive.box('settings').listenable(),
+      builder: (context, box, widget) {
+        var isDarkMode = box.get('darkMode') ?? false;
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Talk to NewsToday Developer',
+              style: TextStyle(
+                fontSize: 42.sp,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: 8),
+            SocialAccounts(color: isDarkMode ? Colors.white : Colors.black87),
+          ],
+        );
+      },
     );
   }
 }
